b9559449b97bb2cb736d871ec94728268dd2cf45
Fabio Ivona <fabio.ivona@defstudio.it>
2023-02-20 10:54:17 +0100

    Update InjectsItself.php

--- src/Concerns/InjectsItself.php
+++ src/Concerns/InjectsItself.php
@@ -7,11 +7,35 @@
 namespace DefStudio\Actions\Concerns;
 
 use DefStudio\Actions\Exceptions\ActionException;
+use \ReflectionClass;
+use \ReflectionParameter;
 
 trait InjectsItself
 {
-    public static function make(array $parameters = []): static
+    public static function make(mixed ...$parameters): static
     {
+        if (empty($parameters)) {
+            return app(static::class);
+        }
+
+        if (!is_array($parameters[0])) {
+            $reflection = new ReflectionClass(static::class);
+
+            if ($reflection->getConstructor()->getNumberOfRequiredParameters() <= count($parameters)) {
+                $parametersNames = collect($reflection->getConstructor()->getParameters())
+                    ->map(fn (ReflectionParameter $parameter) => $parameter->getName())
+                    ->values();
+
+                $newParameters = [];
+                foreach (array_values($parameters) as $index => $parameter) {
+                    $newParameters[$parametersNames->get($index)] = $parameter;
+                }
+
+                $parameters = $newParameters;
+            }
+        }
+
+
         return app(static::class, $parameters);
     }
 
